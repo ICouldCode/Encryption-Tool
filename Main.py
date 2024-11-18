48abdeffb9a053672cadada353f74bd9704e6617
Bryce <133471084+ICouldCode@users.noreply.github.com>
2024-11-18 14:31:35 -0600

    Loading sequence and checking
    You're now able to input your own file path, also checks if the path exists or not as well.

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
 from cryptography.fernet import Fernet
+import os
+import itertools
+import sys
+import time
+import threading
 
 cmd = input("Generate key? [1]yes [2]no\n")
 key = Fernet.generate_key()
@@ -15,19 +20,43 @@ fernet = Fernet(key)
 
 cmd = input("[1]encrpyt [2]decrpyt\n")
 
+while True:
+    filePath = input("Input file path: ")
+    if os.path.exists(filePath):
+        break
+    print("Invalid path, try again\n")
+
 if(cmd == "1"):
-    with open('test.txt', 'rb') as file:
+    with open(filePath, 'rb') as file:
         original = file.read()
     
     encrypted = fernet.encrypt(original)
 
-    with open('test.txt', 'wb') as encryptedFile:
+    with open(filePath, 'wb') as encryptedFile:
         encryptedFile.write(encrypted)
 else:
-    with open('test.txt', 'rb') as encFile:
+    with open(filePath, 'rb') as encFile:
         encrypted = encFile.read()
 
     decrypted = fernet.decrypt(encrypted)
 
-    with open('test.txt', 'wb') as decFile:
+    with open(filePath, 'wb') as decFile:
         decFile.write(decrypted)
+
+done = False
+
+def animation():
+        for c in itertools.cycle(['|', '/', '-', '\\']):
+            if done:
+                break
+            sys.stdout.write('\rloading ' + c)
+            sys.stdout.flush()
+            time.sleep(0.1)
+        sys.stdout.write('\Complete!     ')
+
+t = threading.Thread(target=animation)
+t.start()
+
+#long process here
+time.sleep(2)
+done = True
